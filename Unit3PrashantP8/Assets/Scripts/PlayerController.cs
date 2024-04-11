0587cbfb45a59b682b27be67bc29241265b932a0
Prashant <751769@student.dusd.net>
2024-04-11 15:26:27 -0700

    Done with 3.2
    Starting 3.3 Don't Just Stand There

--- Unit3PrashantP8/Assets/Scripts/PlayerController.cs
+++ Unit3PrashantP8/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRB;
     public float jumpForce = 10;
     public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
         }
     }
 }
